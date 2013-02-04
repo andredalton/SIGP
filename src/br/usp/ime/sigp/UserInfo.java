0b53a81ed42491922fd2230e0d30d8f279d83344
Andre Meneghelli Vale <andre@kakaroto.(none)>
2013-02-04 15:16:44 -0200

    arrumando bug de SESSAO e modificando o persistence.xml

---
+++ src/br/usp/ime/sigp/UserInfo.java
@@ -0,0 +1,17 @@
+package br.usp.ime.sigp;
+
+import br.usp.ime.sigp.modelo.Usuario;
+
+public class UserInfo {
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
