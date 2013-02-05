26c6d6ffdeb5bed8a1787e8ea5ebb2e4446fdd6a
Andre Meneghelli Vale <andre@kakaroto.(none)>
2013-02-05 09:03:24 -0200

    Inicio da parte administrativa de Research Lines

--- WebContent/WEB-INF/jsp/lines/list.jsp
+++ WebContent/WEB-INF/jsp/lines/list.jsp
@@ -1,12 +1,21 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
-</html>
+<table>
+	<thead>
+		<tr>
+			<th>Nome</th>
+		</tr>
+	</thead>
+	<tbody>
+		<c:forEach items="${lines}" var="line">
+			<tr>
+				<td>${line.id.nome}</td>
+				<form action="<c:url value="/line/edit/${line.id.nome}"/>">
+					<td><input type="submit" value="Editar" /></td>
+				</form>
+				<form action="<c:url value="/line/delete/${line.id.nome}"/>" method="POST">
+					<input type="hidden" name="_method" value="DELETE" />
+					<td><input type="submit" value="Remover" /></td>
+				</form>
+			</tr>
+		</c:forEach>
+	</tbody>
+</table>
--- src/br/usp/ime/sigp/controller/LinesController.java
+++ src/br/usp/ime/sigp/controller/LinesController.java
@@ -8,23 +8,54 @@ import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.usp.ime.sigp.dao.GenericDAOString;
 import br.usp.ime.sigp.jpa.BaseEntityString;
+import br.usp.ime.sigp.modelo.LinhadePesquisa;
 import br.usp.ime.sigp.modelo.Marcador;
+import br.usp.ime.sigp.modelo.Topico;
+import br.usp.ime.sigp.modelo.TopicoPk;
 
 @Resource
 public class LinesController {
-    private final GenericDAOString dao;
-    private final Result result;
+	private final GenericDAOString dao;
+	private final Result result;
 
-    public LinesController(GenericDAOString dao, Result result) {
-         this.result   = result;
-         this.dao      = dao;
-    }
+	public LinesController(GenericDAOString dao, Result result) {
+		this.result = result;
+		this.dao = dao;
+	}
 
-    @Path({"/lines/", "/lines"})
-    public void index() {
-    }
-    
-    @Path("/lines/previous")
-    public void previous_lines() {
-    }
+	@Path({ "/lines/", "/lines" })
+	public void index() {
+	}
+
+	@Path("/lines/previous")
+	public void previous_lines() {
+	}
+
+	@Path("/lines/list")
+	public void list() {
+		List<BaseEntityString> lines = dao.selectByNamedQuery("selectLines");
+		result.include("lines", lines);
+		/* result.include("years", this.pdao.years_with_publication()); */
+	}
+	
+	@Path("/line/delete/{line.nome}")
+	public void delete(String nome){
+		/*
+		LinhadePesquisa line = new LinhadePesquisa();
+		line.setNome(nome);
+		line = dao.selectByNamedQuery(namedQuery);
+		
+			TopicoPk topicopk = new TopicoPk();
+			topicopk.setNome(nome);
+			topicopk.setMarcadorpai(marcadorpai);
+			Topico topico = new Topico();
+			topico.setId(topicopk);
+			topico = dao.selectById(topico);
+			if (topico != null && topico.getId() != null) {
+				dao.remove(topico);
+			}
+			list();
+			result.redirectTo(this.getClass()).list();
+		*/
+	}
 }
